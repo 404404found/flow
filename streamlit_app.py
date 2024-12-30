0f950def0b8296bfb6ee11fc48b457e5e1a264c1
404404found <404404found@gmail.com>
2024-12-30 14:07:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,27 @@
 import streamlit as st
+from langflow.load import run_flow_from_json
 
-# Load your LangFlow configuration JSON file
-flow_file_path = "Basic Prompting.json"  # Replace with your file path
+
+TWEAKS = {
+  "ChatInput-tyg1u": {},
+  "Prompt-UfgZi": {},
+  "OpenAIModel-A4s4A": {},
+  "ChatOutput-VmLIe": {}
+}
+
+                            
 
 # Initialize the LangFlow pipeline
 st.title("LangFlow Application")
 try:
     user_input = st.text_input("Enter your query:")
+    result = run_flow_from_json(flow="Basic Prompting.json",
+                            input_value=user_input,
+                            session_id="", # provide a session id if you want to use session state
+                            fallback_to_env_vars=True, # False by default
+                            tweaks=TWEAKS)
     if user_input:
-        st.write("Response:")
+        response = pipeline.run(user_input)
+        st.write("Response:", response)
 except Exception as e:
     st.error(f"Error loading the LangFlow configuration: {e}")
